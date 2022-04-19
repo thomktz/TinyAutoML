8402f79009ec22dd607fa4b64c8a161474ea9120
Thomas Kientz <thomas@kientz.net>
2022-04-19 20:21:28 +0200

    __init__ fix

--- TinyAutoML/Models/__init__.py
+++ TinyAutoML/Models/__init__.py
@@ -1,9 +1,11 @@
-from MetaModels.DemocraticModel import DemocraticModel
-from MetaModels.BestModel import BestModel
-from MetaModels.OneRulerForAll import OneRulerForAll
+import os
+print(os.getcwd())
+from .MetaModels.DemocraticModel import DemocraticModel
+from .MetaModels.BestModel import BestModel
+from .MetaModels.OneRulerForAll import OneRulerForAll
 
-from EstimatorPools.EstimatorPool import EstimatorPool
-from EstimatorPools.EstimatorPoolCV import EstimatorPoolCV
-from MetaModel import MetaModel
+from .EstimatorPools.EstimatorPool import EstimatorPool
+from .EstimatorPools.EstimatorPoolCV import EstimatorPoolCV
+from .MetaModel import MetaModel
 
 __all__ = ['DemocraticModel', 'BestModel', 'OneRulerForAll', 'EstimatorPool', 'EstimatorPoolCV', 'MetaModel']
